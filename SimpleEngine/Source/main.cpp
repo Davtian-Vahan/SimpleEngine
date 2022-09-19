4477823a9a0dc2d0a5cafeac1e13685fdef1a674
Vahan Davtyan <jedavtian@gmail.com>
2022-09-19 23:47:52 +0400

    SMFL setup and initial code

--- SimpleEngine/Source/main.cpp
+++ SimpleEngine/Source/main.cpp
@@ -1,8 +1,29 @@
-#include <iostream>
+// Define SFML to be linked as a static libary with the exe
+#define SFML_STATIC 
+
+// Relevant includes for SFML graphics
+#include <SFML/Graphics.hpp>
+
 
 int main()
 {
-	std::cout << "Initial code";
+	sf::RenderWindow window(sf::VideoMode(1280, 960), "Test Code");
+	sf::RectangleShape  rect_shape({50, 50});
+	rect_shape.setFillColor(sf::Color::Red);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(rect_shape);
+		window.display();
+	}
 
 	return 0;
 }
